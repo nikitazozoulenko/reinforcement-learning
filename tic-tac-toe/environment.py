fixed backprop to only max on allowed actions

tic-tac-toe/environment.py
@@ -41,7 +41,8 @@ class GameBoard:
 
     
     def get_allowed_actions(self):
-        return np.where(self.board.reshape(-1)==0)[0]
+        '''Returns np.array of size (-1). True == allowed'''
+        return (self.board.reshape(-1) == 0).astype(np.uint8)
 
     
     def reverse_player_positions(self):

tic-tac-toe/mcts.py
@@ -57,6 +57,13 @@ class MCTS:
 
     def reset(self):
         self.root = Node(network=self.network, s=GameBoard(self.board_size, self.win_length), parent=None, prev_a=None, depth=0, is_terminate_state=False)
+
+    
+    def get_original_root(self):
+        node = self.root
+        while node.parent != None:
+            node = node.parent
+        return node
         
 
 class Node:
@@ -68,6 +75,7 @@ class Node:
         self.depth = depth
         self.is_terminate_state = is_terminate_state
 
+        self.allowed_actions = torch.from_numpy(self.s.get_allowed_actions())
         self.tree_Q = self.network(board_to_tensor(s))[0]
         self.children = [None] * self.tree_Q.size(-1)
 
@@ -117,9 +125,9 @@ class Node:
         self.children[a] = Node(s=s_prime.reverse_player_positions(), network=self.network, parent=self, prev_a=a, depth=self.depth+1, is_terminate_state=terminate)
         if terminate:
             estimated_return = torch.tensor(r).float().to(device)
-            #self.children[a].tree_Q.zero_()
+            self.children[a].tree_Q.zero_()
         else:
-            estimated_return = r-torch.max(self.children[a].tree_Q.data)
+            estimated_return = r-torch.max(self.children[a].tree_Q.data[self.children[a].allowed_actions])
         self.tree_Q[a] = estimated_return
         self.backpropagate()
         
@@ -127,7 +135,7 @@ class Node:
     def backpropagate(self):
         if self.parent != None:
             old_q_value = self.parent.tree_Q[self.prev_a]
-            new_q_value = -torch.max(self.tree_Q)
+            new_q_value = -torch.max(self.tree_Q[self.allowed_actions])
             if old_q_value != new_q_value:
                 self.parent.tree_Q[self.prev_a] = new_q_value
                 self.parent.backpropagate()

tic-tac-toe/network.py
@@ -7,13 +7,14 @@ class FCC(nn.Module):
         layers = [nn.Linear(size*size, channels),
                   nn.ReLU(inplace = True),
                   nn.BatchNorm1d(channels),
-                  nn.Dropout(0.1)]
+                  nn.Dropout(0.05)]
         for _ in range(n_layers):
             layers += [nn.Linear(channels, channels),
                        nn.ReLU(inplace = True),
                        nn.BatchNorm1d(channels),
-                       nn.Dropout(0.1)]
+                       nn.Dropout(0.05)]
         layers += [nn.Linear(channels, size*size)]
+        layers += [nn.Tanh()]
         self.fcc = nn.Sequential(*layers)
 
 
@@ -21,38 +22,40 @@ class FCC(nn.Module):
         return self.fcc(x)
 
 
-# class CNN(nn.Module):
-#     def __init__(self):
-#         super(CNN, self).__init__()
-#         conv = [nn.BatchNorm2d(36),
-#                   nn.Conv2d(36, 64, kernel_size=3, stride=1, padding=1),
-#                   nn.ReLU(inplace = True)]
-#         for _ in range(3):
-#             conv += [ResidualBlock(32, expansion=2, cardinality=1)]
-#         self.conv = nn.Sequential(*conv)
-#         self.linear = nn.Linear(32*2*3*3, 12)
+class CNN(nn.Module):
+    def __init__(self, size=5, channels=64, n_layers=5):
+        super(CNN, self).__init__()
+        self.size = size
+        self.channels = channels
+        conv = [nn.Conv2d(1, channels, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(inplace = True)]
+        for _ in range(n_layers):
+            conv += [ResidualBlock(channels//2, expansion=2, cardinality=1)]
+        self.conv = nn.Sequential(*conv)
+        self.linear = nn.Sequential(nn.Linear(size*size*channels, size*size),
+                                    nn.Tanh())
 
 
-#     def forward(self, x):
-#         x = self.conv(x)
-#         x = self.linear(x.view(-1, 32*2*3*3))
-#         return x
+    def forward(self, x):
+        x = self.conv(x.view(-1, self.size, self.size, 1))
+        x = self.linear(x.view(-1, self.size*self.size*self.channels))
+        return x
 
 
-# class ResidualBlock(nn.Module):
-#     def __init__(self, channels, expansion = 4, cardinality = 1):
-#         super(ResidualBlock, self).__init__()
-#         self.block = nn.Sequential(nn.Conv2d(channels*expansion, channels, kernel_size=1, bias=False),
-#                                    nn.BatchNorm2d(channels),
-#                                    nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups = cardinality, bias=False),
-#                                    nn.BatchNorm2d(channels),
-#                                    nn.Conv2d(channels, channels*expansion, kernel_size=1, bias=False),
-#                                    nn.BatchNorm2d(channels*expansion))
-#         self.relu = nn.ReLU(inplace = True)
+class ResidualBlock(nn.Module):
+    def __init__(self, channels, expansion = 4, cardinality = 1):
+        super(ResidualBlock, self).__init__()
+        self.block = nn.Sequential(nn.Conv2d(channels*expansion, channels, kernel_size=1, bias=False),
+                                   nn.BatchNorm2d(channels),
+                                   nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups = cardinality, bias=False),
+                                   nn.BatchNorm2d(channels),
+                                   nn.Conv2d(channels, channels*expansion, kernel_size=1, bias=False),
+                                   nn.BatchNorm2d(channels*expansion))
+        self.relu = nn.ReLU(inplace = True)
         
         
-#     def forward(self, x):
-#         res = x
-#         out = self.block(x)
-#         out = self.relu(out+res)
-#         return out
+    def forward(self, x):
+        res = x
+        out = self.block(x)
+        out = self.relu(out+res)
+        return out

tic-tac-toe/train.py
@@ -10,16 +10,16 @@ import torch.optim as optim
 
 from network import FCC
 from environment import GameBoard, step
-from graphing import graph, values2ewma
 from mcts import MCTS, eps_greedy, board_to_tensor
+from graph import graph, ewma
 
 device = torch.device("cuda")
 model_path="save_dir/model.pth"
 
 class ExperienceReplay:
     def __init__(self, maxlen):
-        self.deque = collections.deque(maxlen=maxlen)
         self.maxlen = maxlen
+        self.reset()
     
 
     def add(self, sars):
@@ -36,6 +36,10 @@ class ExperienceReplay:
         samples = random.sample(self.deque, size)
         return samples
 
+    
+    def reset(self):
+        self.deque = collections.deque(maxlen=self.maxlen)
+
 
 class Player:
     def __init__(self, mcts, experience_replay):
@@ -51,10 +55,18 @@ class Player:
         self.mcts.change_root_with_action(action)
 
     
-    def add_to_experience_replay(self):
-        s = board_to_tensor(self.mcts.root.s)[0]
-        tree_Q = self.mcts.root.tree_Q.data
-        self.experience_replay.add([s, tree_Q])
+    # def add_to_experience_replay(self):
+    #     s = board_to_tensor(self.mcts.root.s)[0]
+    #     tree_Q = self.mcts.root.tree_Q.data
+    #     allowed_actions = self.mcts.root.allowed_actions
+    #     self.experience_replay.add([s, tree_Q, allowed_actions])
+
+
+    def add_to_experience_replay(self, node):
+        s = board_to_tensor(node.s)[0]
+        tree_Q = node.tree_Q.data
+        allowed_actions = node.allowed_actions
+        self.experience_replay.add([s, tree_Q, allowed_actions])
 
 
 class MatchHandler:
@@ -74,6 +86,11 @@ class MatchHandler:
         agent_starts = bool(random.randint(0,1))
         if not agent_starts:
             players.reverse()
+        if do_print:
+            if agent_starts:
+                print("AGENT STARTS")
+            else:
+                print("OPPONENT STARTS")
 
         #turn loop until terminate
         _ = players[1].monte_carlo_tree_search(max_mcts_steps, mcts_eps, final_choose_eps) #player waiting gets to start thinking first
@@ -85,36 +102,46 @@ class MatchHandler:
 
         #evaluate who won and tally results
         if coords:
-            board_value = int(self.game_board.board[coords[0]])
-            is_cross = True if board_value==1 else False
-            if is_cross == agent_starts:
+            cross_won = self.game_board.board[coords[0]] > 0
+            if cross_won == agent_starts:
                 self.wins_agent += 1
             else:
                 self.losses_agent += 1
         else:
             self.draws_agent += 1
 
+        #traverse tree backwards and add to experience replay
+        self.traverse_and_add_to_replay()
+
+
+    def traverse_and_add_to_replay(self):
+        node = self.agent.mcts.root
+        while node.parent != None:
+            node = node.parent
+            self.agent.add_to_experience_replay(node)
+        self.agent.add_to_experience_replay(node)
+
 
     def play_turn(self, player, opponent, max_mcts_steps, mcts_eps, final_choose_eps, do_print):
         picked_action = player.monte_carlo_tree_search(max_mcts_steps, mcts_eps, final_choose_eps)
         self.game_board.take_action(picked_action)
-
-        for person in [player, opponent]:
-            if person.experience_replay != None:
-                person.add_to_experience_replay()
-            person.change_root_with_action(picked_action)
-
+        player.change_root_with_action(picked_action)
+        opponent.change_root_with_action(picked_action)
+        # for person in [player, opponent]:
+        #     if person.experience_replay != None:
+        #         person.add_to_experience_replay()
+        #     person.change_root_with_action(picked_action)
         terminate, coords = self.game_board.check_win_position()
 
         if do_print:                
             print("game board")
             print(terminate, coords)
             print(self.game_board)
-            print()
+            print(player.mcts.root.tree_Q)
             print()
         return terminate, coords
 
-    
+
     def reset_tally_results(self):
         self.wins_agent = 0
         self.draws_agent = 0
@@ -122,36 +149,45 @@ class MatchHandler:
     
 
 class OptimizerHandler:
-    def __init__(self, match_handler, learning_rate=0.00001):
+    def __init__(self, match_handler, batch_size, n_iter_train, learning_rate):
         self.match_handler=match_handler
+        self.batch_size = batch_size
+        self.n_iter_train = n_iter_train
         self.learning_rate = learning_rate
         self.reset_optim_after_opponent_update = True
         self.MSE = nn.MSELoss()
         self.create_optim()
+
+        self.optim_counter = 0
+        self.losses = []
         
 
     def create_optim(self):
-        self.optimizer = optim.RMSprop(self.match_handler.agent.mcts.network.parameters(), weight_decay=0.00001, lr=self.learning_rate)
+        self.optimizer = optim.SGD(self.match_handler.agent.mcts.network.parameters(), weight_decay=0.001, lr=self.learning_rate, momentum=0.9, nesterov=True)
+        # self.optimizer = optim.Adam(self.match_handler.agent.mcts.network.parameters(), weight_decay=0.001, lr=self.learning_rate)
 
 
-    def optimize_model(self, n_train=100, batch_size=128):
+    def optimize_model(self):
         model = self.match_handler.agent.mcts.network
         experience_replay = self.match_handler.agent.experience_replay
         model.train()
-        for i in range(n_train):
-            samples = experience_replay.sample()
-            s, target = zip(*samples)
+        first_loss = None
+        for _ in range(self.n_iter_train):
+            samples = experience_replay.sample(self.batch_size)
+            s, target, allowed_a = zip(*samples)
             s = torch.stack(s)
             target = torch.stack(target)
-            
+            allowed_a = torch.from_numpy(np.stack(allowed_a))#.to(device)
+
             self.optimizer.zero_grad()
-            loss = self.MSE(model(s), target)
+            loss = self.MSE(model(s)[allowed_a], target[allowed_a])
+            first_loss = loss.data.cpu().numpy() if first_loss == None else first_loss
             loss.backward()
             self.optimizer.step()
         model.eval()
-        ##TODO TODO TODO
-        # add loss tracking with graphs
-        ##TODO TODO TODO
+
+        self.losses += [(self.optim_counter, first_loss, loss.data.cpu().numpy())]
+        self.optim_counter += 1
 
     
     def update_opponent_if_needed(self, min_n_games=500, max_n_games=2000):
@@ -176,7 +212,8 @@ class OptimizerHandler:
         agent_network.load_state_dict(torch.load(model_path))
         self.match_handler.reset_tally_results()
         if self.reset_optim_after_opponent_update:
-           self.create_optim()
+            self.create_optim()
+            self.match_handler.agent.experience_replay.reset()
     
 
     def update_opponent(self):
@@ -187,6 +224,7 @@ class OptimizerHandler:
         self.match_handler.reset_tally_results()
         if self.reset_optim_after_opponent_update:
             self.create_optim()
+            self.match_handler.agent.experience_replay.reset()
 
 
 def create_agent_and_opponent(board_size, win_length, replay_maxlen):
@@ -211,25 +249,36 @@ def create_agent_and_opponent(board_size, win_length, replay_maxlen):
 
 def main():
     #variables
-    board_size = 5
-    win_length = 4
+    board_size = 3
+    win_length = 3
     max_mcts_steps=10
-    mcts_eps=0.1
+    mcts_eps=0.05
     final_choose_eps=0
-    replay_maxlen = 5000
+    replay_maxlen = 2500
+    batch_size = 256
+    n_iter_train = 10
+    learning_rate = 0.001
+    min_n_games=100
+    max_n_games=200
 
     #match handler
     match_handler = MatchHandler(*create_agent_and_opponent(board_size, win_length, replay_maxlen))
-    optimizer_handler = OptimizerHandler(match_handler)
+    optimizer_handler = OptimizerHandler(match_handler, batch_size, n_iter_train, learning_rate)
 
     #play some games
     for i in range(100000):
         match_handler.play_match(max_mcts_steps, mcts_eps, final_choose_eps)
+        optimizer_handler.optimize_model()
+        print()
         print("wins", match_handler.wins_agent)
         print("draws", match_handler.draws_agent)
         print("losses", match_handler.losses_agent)
-        optimizer_handler.optimize_model()
-        optimizer_handler.update_opponent_if_needed()
+        print("LOSS", optimizer_handler.losses[-1][-1])
+        optimizer_handler.update_opponent_if_needed(min_n_games, max_n_games)
+
+        if not i % 100 and i:
+            graph(ewma(np.array(optimizer_handler.losses)[:, -1], alpha=0))
+
 
 
 if __name__ == "__main__":

tic-tac-toe/try_agent.py
@@ -10,7 +10,6 @@ import torch.optim as optim
 
 from network import FCC
 from environment import GameBoard, step
-from graphing import graph, values2ewma
 from mcts import MCTS, eps_greedy, board_to_tensor
 from train import model_path, create_agent_and_opponent, MatchHandler
 
@@ -18,8 +17,8 @@ def main():
     #variables
     board_size = 3
     win_length = 3
-    max_mcts_steps=800
-    mcts_eps=0.1
+    max_mcts_steps=100
+    mcts_eps=0.05
     final_choose_eps=0
     replay_maxlen = 5000
 